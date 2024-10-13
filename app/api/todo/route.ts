a8b48c12e790b905f9ea84adc0696d4f59b72a9f
최민혁 <nogoso3@gmail.com>
2024-10-14 00:07:55 +0900

    Fix: getTodosList -> getTodos 수정

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -1,8 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getTodoList } from "@/actions/todos/todos.actions";
+import { getTodos } from "@/actions/todos/todos.actions";
 
 export const GET = (req: NextRequest) => {
   // ServerAction 함수를 호출한다.
-  const data = getTodoList(); // 서버에서도 ServerAction 함수 호출 가능하다.
+  const data = getTodos(); // 서버에서도 ServerAction 함수 호출 가능하다.
   return NextResponse.json({ ...data });
 };
