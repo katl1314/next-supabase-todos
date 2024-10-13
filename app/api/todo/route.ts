b2c3c72e0ce17ba50fc1c17c7159eb220e1fe1d9
최민혁 <nogoso3@gmail.com>
2024-10-13 16:02:36 +0900

    Feat: 주석 삭제

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -1,13 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getTodoList } from "@/actions/todos/todos.actions";
 
-// export const GET = async (req: NextRequest, res: NextResponse) => {
-//   const supabase = createServerSideClient();
-//   const { data, error } = await supabase.from("todos_no_rls").is("deleted_at", null).select("*");
-//   // todo 목록 조회
-//   return NextResponse.json({ data, error });
-// };
-
 export const GET = (req: NextRequest) => {
   // ServerAction 함수를 호출한다.
   const data = getTodoList(); // 서버에서도 ServerAction 함수 호출 가능하다.
