7bc2a9ee653d4c160287257032c7235655ae860e
최민혁 <nogoso3@gmail.com>
2024-10-14 22:59:07 +0900

    Fix: 불필요한 import로직 삭제, public.profiles 테이블 typescript types 생성

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import AuthHeader from "@/components/AuthHeader";
-import { createServerSideClient } from "@/lib/server/supabase";
 import { getUserInfo } from "@/actions/user/user.actions";
 
 const inter = Inter({ subsets: ["latin"] });
